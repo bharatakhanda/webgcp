22d1d5f8e50ed7480d1e37939ad55487c791db07
bharatakhanda <47316980+bharatakhanda@users.noreply.github.com>
2021-04-24 17:00:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen()
+const fastify = require('fastify')({logger: true})
+  
+fastify.get('/', async (request, reply) => {
+    reply.status(200).send ({ message: 'Welcome to Fastify' })
+    })
+  
+fastify.listen({
+  port: 0,
+  host: '0.0.0.0',
+  exclusive: false,
+  readableAll: false,
+  writableAll: false,
+  ipv6Only: false
+}, (err) => {})
